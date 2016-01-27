0afe1d8f2f58af6a816d7543d2f5c6f6d0f81c42
John Youn <yundhun@naver.com>
2016-01-27 23:10:36 +0900

    first commit

---
+++ ui/lib/jellyfish.js
@@ -0,0 +1,299 @@
+ $(function() {
+    // there's the toDoGround and the completeGround
+    var $toDoGround = $( "#toDoGround" ),      
+        $completeGround = $( "#completeGround" );    
+
+    //수정상자 팝업 기능 부여.
+    $( "#dialog" ).dialog({autoOpen: false, modal: true});
+
+    //수정상자에서 '수정완료' 클릭시 이벤트.
+    $( "#dialog" ).children('.modComplete').click(function(){
+      var uuid = $( "#dialog" ).children('.uuid').val(); //uuid
+      var receiver = $( "#dialog" ).children('.receiver').val(); //receiver ID
+      var contents = $( "#dialog" ).children('.modContents').val(); //변경 내용
+      if(receiver.length == 0){
+        alert("받는 사람을 입력해 주세요.");
+      }else{
+        postUpdate(uuid, userName,receiver,contents,"DNA", "DNA");        
+      }      
+    });
+
+    //수정상자에서 '삭제' 클릭시 이벤트.
+    $( "#dialog" ).children('.removeToDo').click(function(){
+      var uuid = $( "#dialog" ).children('.uuid').val(); //uuid
+      postDelete(uuid);
+    });    
+
+    //로그아웃 클릭시 이벤트
+    $("#logout").click(function(){
+      Cookies.set("jellyfishId", "DNA", { path:"/"});
+      window.location.href = "login.html";
+    });
+
+    //할일 추가하기 클릭시 이벤트
+    $("#makeToDo").click(function(){
+      var uuid  = guid();
+      $( "#dialog" ).children('.uuid').val(uuid);
+      $( "#dialog" ).children('.receiver').val("");
+      $( "#dialog" ).children('.modContents').val("");
+      $( "#dialog" ).dialog( "open");
+      $( "#dialog" ).children('.receiver').focus();
+    });
+ 
+    // '다했어요' 디비전에 설정.
+    $completeGround.droppable({
+      accept: "#toDoGround > li",
+      drop: function( event, ui ) {
+        completeGroundFunc( ui.draggable );
+      }
+    });
+ 
+    // let the toDoGround be droppable as well, accepting items from the completeGround
+    $toDoGround.droppable({
+      accept: "#toDoGround > li",
+      drop: function( event, ui ) {
+        toDoGroundFunc( ui.draggable );
+      }
+    });
+ 
+    //
+    function completeGroundFunc( $item ) {
+      //$item.css("background-color","Salmon");
+      var uuid = $item.children('.uuid').text(); //uuid
+      var jellyOrfish = $item.children('.jellyOrFish').text();
+      if(jellyOrfish=="(Jelly)"){
+        var status = $item.children('.statusReceiver').text();
+        if(status!="O"){
+          postUpdate(uuid,"DNA","DNA","DNA","DNA","O");
+        }        
+      }else{
+        var status = $item.children('.statusSender').text();
+        if(status!="O"){
+          postUpdate(uuid,"DNA","DNA","DNA","O","DNA");
+        }
+        
+      }
+    }
+ 
+    //
+    function toDoGroundFunc( $item ) {
+      //$item.css("background-color","white");
+      var uuid = $item.children('.uuid').text(); //uuid
+      var jellyOrfish = $item.children('.jellyOrFish').text(); 
+      var status = "";
+      if(jellyOrfish=="(Jelly)"){
+        var status = $item.children('.statusReceiver').text();
+        if(status!="X"){
+          postUpdate(uuid,"DNA","DNA","DNA","DNA","X");
+        }
+      }else{
+        var status = $item.children('.statusSender').text();
+        if(status!="X"){
+          postUpdate(uuid,"DNA","DNA","DNA","X","DNA");
+        }
+      }
+    }
+ 
+    // resolve the icons behavior with event delegation
+    $( "ul.toDoGround > li" ).click(function( event ) {
+      var $item = $( this ),
+        $target = $( event.target );
+ 
+      if ( $target.is( "a.ui-icon-trash" ) ) {
+        deleteImage( $item );
+      } 
+      else if ( $target.is( "a.ui-icon-refresh" ) ) {
+        //recycleImage( $item );
+      }
+  
+      return false;
+    });
+
+    //할일들을 화면에 뿌린다.
+    function refreshToDos(){
+      postGetToDos();
+    }
+
+    //삭제 한다.
+    function postDelete(uuid){  
+      $.ajax({
+        method: "POST",      
+        url: "/delete",
+        data: { "uuid": uuid },
+        dataType: "json",
+        success:function(data){
+          //console.log(data)
+          refreshToDos();
+          $( "#dialog" ).dialog( "close");
+        },
+        fail:function(receive){
+          console.log('실패 했습니다. 다시 시도해주세요.');
+          //console.log(receive);
+        }
+      });
+    }
+
+    //새로 등록 또는 업데이트 한다.
+    function postUpdate(uuid, sender,receiver,contents,statusSender,statusReceiver){  
+      $.ajax({
+        method: "POST",      
+        url: "/regNew",
+        data: { "uuid": uuid, "sender" : sender, "receiver": receiver, "contents": contents, "statusSender": statusSender, "statusReceiver":statusReceiver },
+        dataType: "json",
+        success:function(data){
+          //console.log(data)
+          refreshToDos();
+          $( "#dialog" ).dialog( "close");
+        },
+        fail:function(receive){
+          console.log('실패 했습니다. 다시 시도해주세요.');
+          //console.log(receive);
+        }
+      });
+    }
+
+    //내 할일을 받아온다.
+    function postGetToDos(){  
+      $.ajax({
+        method: "POST",      
+        url: "/getToDos",
+        data: { "userName": userName },
+        dataType: "json",
+        success:function(toDos){
+
+            //응답값에 존재하지 않는 메모는 삭제된 것이므로 삭제한다.
+            var i = 0;
+            $.each($("#toDoGround").children(".ui-widget-content"), function(){
+             var uuidTmp = $("#toDoGround").children(".ui-widget-content").children(".uuid").eq(i).text();
+             if(uuidTmp=="uuidDefaultValue"){
+              //Do Nothing
+             }else{
+              var tempFlg = true;
+              $.each(toDos, function(index, toDo){
+              if(toDo.uuid==uuidTmp){
+                tempFlg = false;
+              }
+              });
+              if(tempFlg){
+                $("#toDoGround").children(".ui-widget-content").eq(i).remove();
+              }
+             }
+                          
+             i++;
+            });
+
+
+            i = 0;
+            $.each(toDos, function(index, toDo){
+              var uuid = toDo.uuid;
+              var tempFlg = true;
+              var i = 0;
+
+              $.each($("#toDoGround").children(".ui-widget-content"), function(){
+                var uuidTmp = $("#toDoGround").children(".ui-widget-content").children(".uuid").eq(i).text();
+                if(uuid==uuidTmp){
+                  $("#toDoGround").children(".ui-widget-content").children('.uuid').eq(i).text(toDo.uuid);
+                  if(userName == toDo.sender){
+                   $("#toDoGround").children(".ui-widget-content").children('.jellyOrFish').eq(i).text("(Fish)");
+                   $("#toDoGround").children(".ui-widget-content").children('.todoImage').eq(i).attr('src', 'image/fish.png');
+                   $("#toDoGround").children(".ui-widget-content").children('.ui-widget-header').eq(i).text(toDo.receiver); 
+                  }else{
+                   $("#toDoGround").children(".ui-widget-content").children('.jellyOrFish').eq(i).text("(Jelly)");
+                   $("#toDoGround").children(".ui-widget-content").children('.todoImage').eq(i).attr('src', 'image/jelly.png');
+                   $("#toDoGround").children(".ui-widget-content").children('.ui-widget-header').eq(i).text(toDo.sender); 
+                  }
+                 if(toDo.statusReceiver=="O" || toDo.statusSender=="O"){
+                  $("#toDoGround").children(".ui-widget-content").eq(i).css("background-color","Salmon");
+                 }
+                 if(toDo.statusReceiver!="O" && toDo.statusSender!="O"){
+                  $("#toDoGround").children(".ui-widget-content").eq(i).css("background-color","white");
+                 }
+                  $("#toDoGround").children(".ui-widget-content").children('.statusReceiver').eq(i).text(toDo.statusReceiver);
+                  $("#toDoGround").children(".ui-widget-content").children('.statusSender').eq(i).text(toDo.statusSender);
+                  $("#toDoGround").children(".ui-widget-content").children('.contents').eq(i).text(toDo.contents);
+                  tempFlg = false;
+                }
+                i++;
+              });
+              
+              if(tempFlg){
+                $cloneToDo = $("#toDo").clone();
+                $cloneToDo.removeAttr("id");
+                $cloneToDo.removeAttr("hidden");
+                $cloneToDo.children('.uuid').text(toDo.uuid);
+                if(userName == toDo.sender){
+                 $cloneToDo.children('.jellyOrFish').text("(Fish)");
+                 $cloneToDo.children('.todoImage').attr('src', 'image/fish.png');
+                 $cloneToDo.children('.ui-widget-header').text(toDo.receiver);
+                }else{
+                 $cloneToDo.children('.jellyOrFish').text("(Jelly)");
+                 $cloneToDo.children('.todoImage').attr('src', 'image/jelly.png');
+                 $cloneToDo.children('.ui-widget-header').text(toDo.sender); 
+                }              
+                $cloneToDo.children('.contents').text(toDo.contents);
+                $cloneToDo.children('.statusReceiver').text(toDo.statusReceiver);
+                $cloneToDo.children('.statusSender').text(toDo.statusSender);
+                if(toDo.statusReceiver=="O" || toDo.statusSender=="O"){
+                   $cloneToDo.css("background-color","Salmon");
+                }
+                $cloneToDo.appendTo( $("#toDoGround") );
+
+                /*if(toDos.length != i){
+                  $clone = $("#toDo").clone();
+                  $clone.removeAttr('id');
+                  $clone.appendTo( $("#toDoGround") );
+                }
+                $("#toDo").css("background-color","white");
+
+                i++;
+                */
+              }
+            });
+
+            $( "li", $toDoGround ).draggable({containment: "document"});
+            
+            $(".modClick").click(function() {
+             var srcUuid = $(this).parent().children('.uuid').text();
+             var srcReceiver = $(this).parent().children('.ui-widget-header').text();
+             var srcContents = $(this).parent().children('.contents').text();
+             $( "#dialog" ).children('.uuid').val(srcUuid);
+             $( "#dialog" ).children('.receiver').val(srcReceiver);
+             $( "#dialog" ).children('.modContents').val(srcContents);
+             $( "#dialog" ).dialog( "open");
+             $( "#dialog" ).children('.modContents').focus();
+            });
+        },
+
+        fail:function(receive){
+          console.log('실패 했습니다. 다시 시도해주세요.');
+          //console.log(receive);
+        }
+      });
+    }    
+
+    //uuid 생성
+    function guid() {
+      function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+          .toString(16)
+          .substring(1);
+      }
+      return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+    }
+
+    //login 하지 않은 상태: login.html 로 페이지를 전환한다.
+    //login 한 상태       : refreshTodos 함수를 호출하여 할일들을 화면에 뿌린다.
+    var userName = Cookies.get("jellyfishId");    
+    if(typeof userName == 'undefined' || userName == "DNA"){
+      window.location.href = "login.html";
+    }else{
+     $("#userName").text(userName);
+     refreshToDos(); 
+    }
+
+    setInterval(function(){   
+      postGetToDos();
+    }, 10000);
+    
+  });
---
+++ was.py
@@ -0,0 +1,126 @@
+from twisted.web import server, resource, static
+from twisted.internet import reactor, endpoints
+import json
+import sqlite3
+import itertools
+import operator
+import datetime
+
+conn = sqlite3.connect('db.db')
+c = conn.cursor()
+
+class getToDos(resource.Resource):
+    isLeaf = True
+
+    def render_POST(self, request):
+        request.setHeader("content-type", "application/json")
+        userName = (request.args['userName'][0]).decode('utf-8')
+
+        return_keyword = []
+
+        yesterdayDatetime = datetime.datetime.now() - datetime.timedelta(days=1)
+        yesterdayStr = yesterdayDatetime.strftime('%Y%m%d000000')
+
+        #sql_str = "select uuid, sender, receiver, contents, statusSender, statusReceiver from toDo where removeFlag != 'Y' and ( sender='%s' or receiver='%s' ) " %(userName, userName)
+        sql_str = """select uuid, sender, receiver, contents, statusSender, statusReceiver from
+(
+select * from toDo where removeFlag != 'Y' and ( sender='%s' or receiver='%s' ) and statusSender||statusReceiver!='OO'
+union all
+select * from toDo where ( sender='%s' or receiver='%s' ) and statusSender||statusReceiver='OO' and updateDateTime > '%s'
+)""" %(userName, userName, userName, userName, yesterdayStr)
+        
+        #print sql_str
+        try:
+            c.execute(sql_str)
+            results = c.fetchall()
+        except sqlite3.Error, e:
+            print "DB Error %s:" % e.args[0]
+            return 'False'
+
+        for result in results:
+            r = {"uuid":result[0], "sender":result[1], "receiver" : result[2], "contents" : result[3], "statusSender" : result[4], "statusReceiver" : result[5]}
+            #print(r)
+            return_keyword.append(r)        
+
+        return json.dumps(return_keyword)
+
+class regNew(resource.Resource):
+    isLeaf = True
+
+    def render_POST(self, request):
+        request.setHeader("content-type", "application/json")
+        #print request.args
+        uuid = (request.args['uuid'][0]).decode('utf-8')
+        sender = (request.args['sender'][0]).decode('utf-8')
+        receiver = (request.args['receiver'][0]).decode('utf-8')
+        contents = (request.args['contents'][0]).decode('utf-8')
+        statusSender = (request.args['statusSender'][0]).decode('utf-8')
+        statusReceiver = (request.args['statusReceiver'][0]).decode('utf-8')
+
+        todayDatetime = datetime.datetime.now()
+        todayStr = todayDatetime.strftime('%Y%m%d%H%M%S')
+
+        sql_str = ""        
+
+        if(sender=="DNA" and receiver=="DNA" and contents=="DNA"):
+            if(statusSender!="DNA"):
+                sql_str = "update toDo set statusSender='%s' , updateDateTime='%s' where uuid='%s' " %(statusSender, todayStr, uuid)
+            else:
+                sql_str = "update toDo set statusReceiver='%s' , updateDateTime='%s' where uuid='%s' " %(statusReceiver, todayStr, uuid)
+        else:
+            sql_str = "select uuid, receiver from toDo where uuid='%s' " %(uuid)
+            try:
+                c.execute(sql_str)
+                results = c.fetchall()
+            except sqlite3.Error, e:
+                print "DB Error %s:" % e.args[0]
+                return 'False'
+            if(len(results)!=0):                
+                #sql_str = "update toDo set receiver='%s', contents='%s' where uuid='%s' " %(receiver, contents, uuid)
+                sql_str = "update toDo set contents='%s' , updateDateTime='%s' where uuid='%s' " %(contents, todayStr, uuid)
+            else:
+                sql_str = "insert into toDo('uuid','sender','receiver','contents', 'createDateTime' ) values ('%s','%s','%s','%s','%s')" %(uuid, sender, receiver, contents, todayStr)
+
+        try:
+            c.execute(sql_str)
+            conn.commit()
+        except sqlite3.Error, e:
+            print "DB Error %s:" % e.args[0]
+            return 'False'
+
+        #print sql_str
+
+        return json.dumps({"result":"success"})
+
+class delete(resource.Resource):
+    isLeaf = True
+
+    def render_POST(self, request):
+        request.setHeader("content-type", "application/json")
+        uuid = (request.args['uuid'][0]).decode('utf-8')
+
+        todayDatetime = datetime.datetime.now()
+        todayStr = todayDatetime.strftime('%Y%m%d%H%M%S')
+
+        sql_str = "update toDo set removeFlag='Y', updateDateTime='%s' where uuid='%s' " %(todayStr, uuid)
+        print sql_str
+        try:
+            c.execute(sql_str)
+            results = c.fetchall()
+        except sqlite3.Error, e:
+            print "DB Error %s:" % e.args[0]
+            return 'False'
+
+        return json.dumps({"result":"success"})
+
+root = static.File("./ui")
+root.putChild("regNew", regNew())
+root.putChild("getToDos", getToDos())
+root.putChild("delete", delete())
+
+factory = server.Site(root) 
+reactor.listenTCP(80, factory)
+
+print '=Jellyfish='
+
+reactor.run()
